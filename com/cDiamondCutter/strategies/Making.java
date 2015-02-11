1056512a1c99602d56c4a5e1b50a46fc44db321f
CapslockRSPS <capslock_donater@hotmail.com>
2015-02-11 16:11:04 +0000

    Update Making.java

--- com/cDiamondCutter/strategies/Making.java
+++ com/cDiamondCutter/strategies/Making.java
@@ -14,19 +14,26 @@ public class Making implements Strategy {
 
 	@Override
 	public boolean activate() {
-		return Inventory.getCount(Constants.UNCUT_DIAMONDS_ID) >= 1;
+		return Inventory.getCount(Constants.UNCUT_DIAMONDS_ID) == 25;
 	}
 
 	@Override
 	public void execute() {
-
-         Inventory.combine(Constants.CHISEL_ID, Constants.UNCUT_DIAMONDS_ID);
+		Item CHISEL = Inventory.getItem(Constants.CHISEL_ID);
+		Item UNCUT_DIAMOND = Inventory.getItem(Constants.UNCUT_DIAMONDS_ID);
+		Menu.sendAction(447, Constants.CHISEL_ID - 1, CHISEL.getSlot(), 3214, 3);
+		Menu.sendAction(870, Constants.UNCUT_DIAMONDS_ID - 1, UNCUT_DIAMOND.getSlot(), 3214, 1);
+		Menu.sendAction(315, 5029888, 272, 2498, 1);
+		Time.sleep(24000);
+		Menu.sendAction(447, Constants.CHISEL_ID - 1, CHISEL.getSlot(), 3214, 3);
+		Menu.sendAction(870, 1601, 14, 3214, 1);
+		Menu.sendAction(315, Constants.DIAMONDS_ID - 1, 8, 2472, 1);
+		Menu.sendAction(315, Constants.DIAMONDS_ID - 1, 20, 2498, 1);
 		Time.sleep(new SleepCondition() {
 			@Override
 			public boolean isValid() {
 				return Players.getMyPlayer().getAnimation() == -1;
 			}
-		}, 1000);
+		}, 500);
 	}
-
 }
