e90ac11ecb5ed93674bf16cb66fa26e78dc12d57
CapslockRSPS <capslock_donater@hotmail.com>
2015-03-02 23:56:07 +0000

    Update Constants.java

--- com/cDiamondCutter/data/Constants.java
+++ com/cDiamondCutter/data/Constants.java
@@ -2,11 +2,11 @@ package com.cDiamondCutter.data;
 
 public class Constants {
 	
-	public static int CRAFTING_SHOP = 545;
-	public static int INTERFACE_ID = 3824;
-	public static int UNCUT_DIAMONDS_ID = 1618;
-	public static int DIAMONDS_ID = 1602;
-	public static int CHISEL_ID = 1756;
+	public static final int CRAFTING_SHOP = 545;
+	public static final int INTERFACE_ID = 3824;
+	public static final int UNCUT_DIAMONDS_ID = 1618;
+	public static final int DIAMONDS_ID = 1602;
+	public static final int CHISEL_ID = 1756;
 	public static int DO_25 = 0;
 
 }
