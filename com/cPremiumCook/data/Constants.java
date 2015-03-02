5ba49c9175a0d1b3bcef023cc262061be84bac79
CapslockRSPS <capslock_donater@hotmail.com>
2015-03-02 23:45:23 +0000

    Update Constants.java

--- com/cPremiumCook/data/Constants.java
+++ com/cPremiumCook/data/Constants.java
@@ -6,7 +6,7 @@ public class Constants
 	public static final int FIRE_ID = 5249;
 	public static final int BANK_ID = 2213;
 	public static final int INTERFACE_ID = 23350;
-	public static int RAW_FISH;
+	public static int rawFish;
 	public static boolean START = false;
 
 }
