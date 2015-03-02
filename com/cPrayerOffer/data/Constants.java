acc7806f367d44215e0a08068a35eb9d40332efe
CapslockRSPS <capslock_donater@hotmail.com>
2015-03-02 23:41:28 +0000

    Update Constants.java

--- com/cPrayerOffer/data/Constants.java
+++ com/cPrayerOffer/data/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	
 	public static final int BANK_ID = 2213;
 	public static final int INTERFACE_ID = 23350;
-	public static int BONES_ID;
+	public static int bonesId;
 	public static final int ALTAR_ID = 409;
 	public static boolean START = false;
 	
