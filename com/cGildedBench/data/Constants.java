be3685c031502897e7f594d626533187a7028b8a
CapslockRSPS <capslock_donater@hotmail.com>
2015-03-02 23:34:23 +0000

    Fixed naming conversations

--- com/cGildedBench/data/Constants.java
+++ com/cGildedBench/data/Constants.java
@@ -2,12 +2,12 @@ package com.cGildedBench.data;
 
 public class Constants {
 	
-	public static int BANK_ID = 4241;
-	public static int INTERFACE_ID = 23350;
-	public static int MAHOGANY_PLANKS = 8783;
-	public static int CONST_INTERFACE_ID = 26700;
-	public static int BENCH_ID = 15436;
-	public static int GOLD_LEAFS = 8785;
-	public static int DONE_BENCH = 13306;
+	public static final int BANK_ID = 4241;
+	public static final int INTERFACE_ID = 23350;
+	public static final int MAHOGANY_PLANKS = 8783;
+	public static final int CONST_INTERFACE_ID = 26700;
+	public static final int BENCH_ID = 15436;
+	public static final int GOLD_LEAFS = 8785;
+	public static final int DONE_BENCH = 13306;
 	
 }
