1a4e5dd5787705574fd3d925ba42bd42d5084734
CapslockRSPS <capslock_donater@hotmail.com>
2015-03-02 23:47:58 +0000

    Update Constants.java

--- com/cJailSolver/data/Constants.java
+++ com/cJailSolver/data/Constants.java
@@ -2,8 +2,8 @@ package com.cJailSolver.data;
 
 public class Constants {
 	
-	public static int ORES_ID = 2893;
-	public static int MINE_ID = 2093;
-	public static int JAILER_ID = 201;
+	public static final int ORES_ID = 2893;
+	public static final int MINE_ID = 2093;
+	public static final int JAILER_ID = 201;
 
 }
