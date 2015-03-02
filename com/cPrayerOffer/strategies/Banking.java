51abcc7a4a42ddc64961e6e861b33de394d9cf9c
CapslockRSPS <capslock_donater@hotmail.com>
2015-03-02 23:38:59 +0000

    Update Banking.java

--- com/cPrayerOffer/strategies/Banking.java
+++ com/cPrayerOffer/strategies/Banking.java
@@ -31,11 +31,11 @@ public class Banking implements Strategy {
 				}
 			}, 500);
 		} else {
-			SceneObject[] BANK_BOOTH = SceneObjects.getNearest(Constants.BANK_ID);
-			if (BANK_BOOTH.length > 0 && BANK_BOOTH != null
-					&& BANK_BOOTH[0].distanceTo() > 0
+			SceneObject[] bankId = SceneObjects.getNearest(Constants.BANK_ID);
+			if (bankId.length > 0 && bankId != null
+					&& bankId[0].distanceTo() > 0
 					&& Players.getMyPlayer().getAnimation() == -1) {
-				BANK_BOOTH[0].interact(0);
+				bankId[0].interact(0);
 				Time.sleep(new SleepCondition() {
 					@Override
 					public boolean isValid() {
