7bd7aa26da08e797f3c212a233633b1308a69f7a
CapslockRSPS <capslock_donater@hotmail.com>
2015-03-02 23:33:05 +0000

    Removed static sleeps

--- com/cGildedBench/strategies/Making.java
+++ com/cGildedBench/strategies/Making.java
@@ -23,7 +23,12 @@ public class Making implements Strategy {
 	public void execute() {
 		if (Game.getOpenInterfaceId() == Constants.CONST_INTERFACE_ID) {
 			Menu.sendAction(632, 8114, 4, 26702, 2);
-			Time.sleep(3000);
+			Time.sleep(new SleepCondition() {
+					@Override
+					public boolean isValid() {
+						return Inventory.getCount(Constants.MAHOGANY_PLANKS) == 12 && Inventory.getCount(Constants.GOLD_LEAFS) == 12;
+					}
+				}, 500);
 			SceneObject[] DONE_BENCH = SceneObjects.getNearest(Constants.DONE_BENCH);
 			if (DONE_BENCH.length > 0 && DONE_BENCH != null && DONE_BENCH[0].distanceTo() > 0 && Players.getMyPlayer().getAnimation() == -1) {
 				Menu.sendAction(1062, DONE_BENCH[0].getHash(), 63, 42, 2);
@@ -38,7 +43,6 @@ public class Making implements Strategy {
 			SceneObject[] BENCH_ID = SceneObjects.getNearest(Constants.BENCH_ID);
 			if (BENCH_ID.length > 0 && BENCH_ID != null && BENCH_ID[0].distanceTo() > 0 && Players.getMyPlayer().getAnimation() == -1) {
 				BENCH_ID[0].interact(4);
-				Time.sleep(500);
 				Time.sleep(new SleepCondition() {
 					@Override
 					public boolean isValid() {
