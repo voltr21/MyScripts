aba6daad7e12c05a1b247c380c2c8baa2e4f18ae
CapslockRSPS <capslock_donater@hotmail.com>
2015-03-02 23:54:55 +0000

    Update Constants.java

--- com/cFiremaker/data/Constants.java
+++ com/cFiremaker/data/Constants.java
@@ -4,9 +4,9 @@ public class Constants {
 	
 	public static int LOGS_ID;
 	public static int TREE_ID;
-	public static int TINDERBOX_ID = 591;
+	public static int SEED_ID;
+	public static final int TINDERBOX_ID = 591;
 	public static boolean START = false;
 	public static int DO_27 = 0;
-	public static int SEED_ID;
 
 }
