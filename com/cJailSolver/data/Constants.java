c5e7a3de5c42c658d7edcf129b9d5e3b380b1b20
CapslockRSPS <Capslock@iMac-de-Sandro.lan>
2015-03-07 11:01:14 +0000

    Fix

--- com/cJailSolver/data/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.cJailSolver.data;
-
-public class Constants {
-	
-	public static final int ORES_ID = 2893;
-	public static final int MINE_ID = 2093;
-	public static final int JAILER_ID = 201;
-
-}
--- com/cMasterFarmer/data/Constants.java
+++
@@ -1,9 +0,0 @@
-package cMasterFarmer.data;
-
-/**
- * Created by Capslock
- * On 06/03/15
- * With IntelliJ
- */
-public class Constants {
-}
